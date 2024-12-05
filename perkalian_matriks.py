d7dc764b9e902c4b421b3b7acaf31e0855be7c26
Pall <fuadasfal32@gmail.com>
2024-12-05 08:24:47 +0800

    perkalian_matriks.py

---
+++ perkalian_matriks.py
@@ -0,0 +1,37 @@
+
+# Mendefinisikan matriks A dan B
+A = [
+    [-2, 2, 3],
+    [4, -6, 7],
+    [7, 8, 9]
+]
+
+B = [
+    [9, 8, 7],
+    [6, -8, 4],
+    [3, 2, 1]
+]
+
+# Inisialisasi matriks hasil C
+C = [[0, 0, 0], 
+     [0, 0, 0], 
+     [0, 0, 0]]
+
+# Melakukan perkalian matriks
+for i in range(len(A)):
+    for j in range(len(B[0])):
+        for k in range(len(B)):
+            C[i][j] += A[i][k] * B[k][j]
+
+# Menampilkan hasil
+print("Matriks A:")
+for row in A:
+    print(row)
+
+print("Matriks B:")
+for row in B:
+    print(row)
+
+print("Matriks Hasil C:")
+for row in C:
+    print(row)
