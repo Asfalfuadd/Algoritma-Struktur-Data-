7f3e0fad300b11d3996f2c48ffcb8456ece3edc5
Pall <fuadasfal32@gmail.com>
2024-12-05 08:37:29 +0800

    dictionary_sederhana.py

---
+++ dictionary_sederhana.py
@@ -0,0 +1,9 @@
+# Membuat dictionary untuk menyimpan data buku
+buku_dict = {
+    "judul": "Belajar Python",
+    "penulis": "Siti",
+    "tahun": 2022
+}
+
+# Menampilkan informasi buku
+print(f"Judul: {buku_dict['judul']}, Penulis: {buku_dict['penulis']}, Tahun: {buku_dict['tahun']}")
