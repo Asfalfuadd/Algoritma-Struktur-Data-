1f81cb5972ec216282ba26837810304149055495
Pall <fuadasfal32@gmail.com>
2024-12-05 08:09:03 +0800

    luas_lingkaran.py

---
+++ luas_lingkaran.py
@@ -0,0 +1,11 @@
+# Inialisasi 
+r = int(input("Masukkan Jari Jari : "))
+
+# Inisialisasi phi
+phi = 3.14
+
+# Hitung luas lingkaran
+luas = phi * r * r
+
+#Output
+print("Luas lingkaran adalah", luas)
