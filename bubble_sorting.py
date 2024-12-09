6acfc8f3ca3b5c50801b98734cc57637191d0b51
Pall <fuadasfal32@gmail.com>
2024-12-09 18:53:03 +0800

    bubble_sorting.py

--- bubble_sorting.py
+++ bubble_sorting.py
@@ -2,18 +2,18 @@ angka = [2,7,10,9,5,3,6,11,20,19]
 
 banyak_angka = len(angka)
 for i in range(banyak_angka - 1):
-        if angka [i] > angka[i+1]:
-                temp = angka[i]
-                angka[i] = angka[i+1]
-                angka[i+1] = temp
-                for j in range(banyak_angka - 1):
-                        if angka[j] > angka[j+1]:
-                                temp = angka[j]
-                                angka[j] = angka[j+1]
-                                angka[j+1] = temp
-                                for k in range(banyak_angka - 1):
-                                        if angka[k] > angka[k+1]:
-                                                temp = angka[k]
-                                                angka[k] = angka[k+1]
-                                                angka[k+1] = temp
-                                                print(angka)
+    if angka [i] > angka[i+1]:
+        temp = angka[i]
+        angka[i] = angka[i+1]
+        angka[i+1] = temp
+        for j in range(banyak_angka - 1):
+            if angka [j] > angka [j+1]:
+                temp = angka [j]
+                angka [j] = angka [j+1]
+                angka [j+1] = temp
+                for k in range (banyak_angka - 1):
+                    if angka [k] > angka [k+1]:
+                        temp = angka [k]
+                        angka [k] = angka [k+1]
+                        angka [k+1] = temp
+                        print(angka)
